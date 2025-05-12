4219b4a831b84a1339c8f4b619f9ef372fd22a52
arnav-devops <itsincognitos@gmail.com>
2025-05-13 00:10:27 +0530

    Add project files.

---
+++ AZURE-CI-CD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AZURE_CI_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
